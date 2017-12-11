d3c161fd4673a9a696f77a083fa145246052efc8
Alejandro Moya <alejandro_moya_moya@hotmail.com>
2017-12-11 18:01:33 +0100

    Retoques en el main
    Se han añadido comentarios en el codigo y se han perfilado un par de cosas. Preparado para testear el proximo dia de clase. Se han realizado un pequeño tratamiento de errores

--- clienteJavaScript.js
+++ clienteJavaScript.js
@@ -42,20 +42,21 @@ function main() {
 	consola("Cliente iniciado... Enviando mensaje al monitor para arrancar (Mensaje CM1)", "green");
 	// Mensaje de inicio, Cliente --> Monitor
 	var estado = sender(urlMonitor, Create_CM1(idCliente, urlMonitor, ipCliente), urlMonitor);
-	var estoyEnTienda = -1;
 	
+	// Si el mensaje no ha sido mandado paramos la ejecución del programa
 	if (estado != 200){
 		return 1;
 	}
 	
-	consol("Has recibido lo siguiente del monitor: ");
+	consola("Has recibido lo siguiente del monitor: ");
 	consola("IDCliente: " + idCliente);
 	consola("Listado de la compra:");
 	consola(productos);
 	consola("Listado de tiendas conocidas:");
 	consola(tiendasConocidas);
 	
-	if (productos.length == 0 || tiendasConocidas.length == 0){
+	// Comprobamos si hemos recibido productos para comprar, lista de tiendas para visitar o un ID adecuado
+	if (productos.length == 0 || tiendasConocidas.length == 0 || idCliente == 0){
 		consola("ERROR: el monitor no te ha dado bien la lista de productos o las tiendas que conoces.", "red");
 		return 1;
 	}
@@ -72,25 +73,44 @@ function main() {
 	*/
 	
 	var i;
-	
+	var estoyEnTienda = -1;
+	// El agente se conectará a las diferentes tiendas hasta que obtenga todos los productos deseados
 	while(productos.length!= 0){
+		// Indicamos que no estamos en una tienda
 		estoyEnTienda = -1;
 		consola("Tienes que comprar los siguiente productos");
 		consola(productos);
+		// Mientras que no hayamos entrado en una tienda... intentamos entrar en alguna
 		while(estoyEnTienda == -1){
+			// Recorremos el vector de las tiendas conocidas
 			for (i = 0; i< tiendasConocidas.length; i++){
+				// Si la tienda no ha sido visitada
 				if (tiendasConocidas[i].Visitado == 0){
+					// Intentamos entrar a la tienda mandando un mensaje CT1
 					estado = sender(tiendasConocidas[i].Direccion, Create_CT1(idCliente, tiendasConocidas[i].Id, ipCliente), urlMonitor);
+					// Si obtenemos un estado = 101, significa que podemos entrar a la tienda y compramos los productos que necesitamos
 					if (estado==101){
 						consola("Has entrado en la tienda " + tiendasConocidas[i].Direccion);
+						// La marcamos como visitada
 						tiendasConocidas[i].Visitado = 1;
+						// Nos guardamos la posicion donde se encuentra almacenada la tienda en el vector de tiendas conocidas del agente
 						estoyEnTienda=i;
+						// Salimos del for
 						break;
+					// Si obtenemos un estado = 100, la tienda no nos deja entrar, deberemos de probar mas adelante
 					} else if (estado == 100){
 						consola("La tienda " + tiendasConocidas[i].Direccion + " esta ocupada, intentalo mas tarde");
+					// Si el estado es 400, el XML que hemos enviado es erroneo.
 					} else if (estado == 400){
 						consola("ERROR 400, XML mal formado", "red");
+					// Si el estado es 404, la tienda a la que queremos acceder no existe, por lo tanto lo eliminamos de nuestras tiendas conocidas
 					} else if (estado == 404){
+						consola("ERROR 404, tienda no encontrada, eliminando tienda...", "red");
+						tiendasConocidas.splice(i,1);
+						i--;
+					// Si el error es 500, la tienda tiene algun fallo y por lo tanto no puede procesar nuestro mensaje, lo eliminamos del sistema
+					} else if (estado == 500 || estado == -1){
+						consola("ERROR 500 o mensaje recibio erroneo, fallo interno en el tienda, eliminando tienda...", "red");
 						tiendasConocidas.splice(i,1);
 						i--;
 					}
@@ -100,42 +120,80 @@ function main() {
 		
 		consola("Tienes que comprar los siguientes productos: ")
 		consola(productos);
-		// Si quedan productos por comprar, preguntar por nuevas tiendas
-		//if (productos.length != 0){
-		//	var j = 0;
-		//	while(quedanSinVisitar() == false){
-		//		consola("Preguntando nuevas tiendas...");
+		// Si quedan productos por comprar y si quedan tiendas por visitar, preguntamos por nuevas tiendas
+		if (productos.length != 0 && quedanSinVisitar() == true){
+			// Inicializamos un contador de veces que preguntamos a una tienda
+			var j = 0;
+			do{
+				consola("Preguntando nuevas tiendas...");
+				// El agente envia un mensaje (CT4) y pregunta por nuevas tiendas para visitar
 				estado = sender(tiendasConocidas[estoyEnTienda].Direccion, Create_CT4(idCliente, tiendasConocidas[estoyEnTienda].Id, ipCliente), urlMonitor);
-		//		if (j == 20){
-		//			consola("ERROR: el sistema no ha sido capaz de proporcionar nuevas tiendas...", "red");
-		//			break;
-		//		}
-		//		j++;
-		//	}
-		//}
-		consola("Saliendo de la tienda: " + tiendasConocidas[estoyEnTienda].Id)
-		estado = sender(tiendasConocidas[estoyEnTienda].Direccion, Create_CT6(idCliente, tiendasConocidas[estoyEnTienda].Id, ipCliente), urlMonitor);
-		//if (j == 20){
-		//	break;
-		//}
-		if (quedanSinVisitar() == false){
-			break;
+				// El XML que se ha mandado es erroneo
+				if (estado == 400){
+					consola("ERROR 400, XML mal formado", "red");
+				// Si el estado es 404, la tienda a la que queremos acceder no existe, por lo tanto lo eliminamos de nuestras tiendas conocidas
+				} else if (estado == 404){
+					consola("ERROR 404, tienda no encontrada, eliminando tienda...", "red");
+					tiendasConocidas.splice(i,1);
+					estoyEnTienda = -1;
+				// Fallo interno en la tienda, no puede procesar nuestro mensaje, por lo tanto lo eliminamos
+				} else if (estado == 500 || estado == -1){
+					consola("ERROR 500 o mensaje recibio erroneo, fallo interno en el tienda, eliminando tienda...", "red");
+					tiendasConocidas.splice(i,1);
+					estoyEnTienda = -1;
+				}
+				// Una vez llegado a las 20 preguntas, desistimos y continuamos con la ejecución 
+				if (j == 20){
+					consola("ERROR: el sistema no ha sido capaz de proporcionar nuevas tiendas...", "red");
+					break;
+				}
+				j++;
+			while (quedanSinVisitar() == false)}
+		}
+		
+		// Si la ejecucion no ha fallado y seguimos en una tienda, le indicamos que vamos a salir
+		if (estoyEnTienda != -1){
+			consola("Saliendo de la tienda: " + tiendasConocidas[estoyEnTienda].Id)
+			estado = sender(tiendasConocidas[estoyEnTienda].Direccion, Create_CT6(idCliente, tiendasConocidas[estoyEnTienda].Id, ipCliente), urlMonitor);
+			// Error en el XML que hemos enviado
+			if (estado == 400){
+				consola("ERROR 400, XML mal formado", "red");
+			// Si el estado es 404, la tienda a la que queremos acceder no existe, por lo tanto lo eliminamos de nuestras tiendas conocidas
+			} else if (estado == 404){
+				consola("ERROR 404, tienda no encontrada, eliminando tienda...", "red");
+				tiendasConocidas.splice(i,1);
+			// Fallo interno en la tienda, no puede procesar nuestro mensaje, por lo tanto lo eliminamos
+			} else if (estado == 500 || estado == -1){
+				consola("ERROR 500 o mensaje recibio erroneo, fallo interno en el tienda, eliminando tienda...", "red");
+				tiendasConocidas.splice(i,1);
+			}
 		}
 	}
 	
 	
 	consola("Enviando mensaje al monitor para finalizar el cliente (Mensaje CM3)");
+	// El agente comunica al monitor que ha finalizado su ejecucion
 	var estado = sender(urlMonitor, Create_CM3(idCliente, urlMonitor, ipCliente), urlMonitor);
-	consola("Cliente ha terminado satisfactoriamente: gracias por jugar");
+	consola("Has terminado con la siguiente informacion: ");
+	consola("IDCliente: " + idCliente);
+	consola("Listado de la compra:");
+	consola(productos);
+	consola("Listado de tiendas conocidas:");
+	consola(tiendasConocidas);
+	consola("El cliente ha terminado la ejecucion satisfactoriamente: gracias por jugar");
 	return 0;
 }
 
+// Funcion que nos devuelve si queda alguna tienda por visitar. True == queda alguna, False == todas han sido visitadas
 function quedanSinVisitar(){
+	// Recorrer todo el vector hasta que encontremos una tienda que no ha sido visitada
 	for(var i=0; i < tiendasConocidas.length; i++){
+		// Si la encontramos devolvemos que si
 		if (tiendasConocidas[i].Visitado == 0){
 				return true;
 			}
 	}
+	// Si no la encontramos devolvemos que no
 	return false;
 }
 
@@ -151,50 +209,58 @@ function sender(direccion, mensaje, dirMonitor) {
 		async: false,
 		dataType: 'text',
 		contentType: 'text/xml',
-
+		
+		// Funcion que se ejecuta antes de enviar el mensaje
 		beforeSend: function(request) {
 			consola("Mandando mensaje a: " + direccion);
 			consola("Mensaje enviado: " + mensaje);
 		},
 
+		// Funcion que se ejecuta si se ha recibido respuesta del agente
 		success: function(response) {
 			consola("Mensaje recibido de " + direccion + ": " + response, "gren");
 			estado=200; //Acierto, todo va bien
-			// Dado que el mensaje se ha enviado correctamente, se replica al monitor
-			// Solo se replicara si el destinatario del mensaje no era el monitor
-			
+			// Dado que el mensaje se ha enviado correctamente, se procede a procesar la respuesta obtenida
 			var parser = new DOMParser();
+			// Se transforma el mensaje al formato XML
 			var response_xml = parser.parseFromString(response,"text/xml");
+			// Se mira la raiz del mensaje, con el fin de saber de que se trata el mensaje recibido
 			var raiz = response_xml.documentElement.tagName
+			// Mensaje de respuesta a CM1 (Se obtiene la ID, la lista de tiendas y la lista de productos)
 			if (raiz == "MC2"){
 				parser_MC2(response_xml);
 			}
+			// Mensaje de respuesta a CM3 (No hay que hacer nada)
 			else if (raiz == "MC4"){
 				parser_MC4(response_xml);
 			}
+			// Mensaje de respuesta a CT1 (Se nos indica que la tienda esta ocupada, no se trata nada y se retorna el codigo de error 100)
 			else if (raiz == "TC2"){
 				parser_TC2(response_xml);
 				estado = 100; // Codigo de acierto 100: tienda ocupada 
 			}
+			// Mensaje de respuesta a CT1 (Se nos indica que la tienda esta libre, se nos da los productos que hemos comprado y se retorna el codigo 101)
 			else if (raiz == "TC3"){
 				parser_TC3(response_xml);
 				estado = 101; // Codigo de acierto 101: tienda no ocupada, has comprado 
 			}
+			// Mensaje de respuesta a CT4 (Se nos indica las nuevas tiendas que podemos visitar)
 			else if (raiz == "TC5"){
 				parser_TC5(response_xml);
 			}
+			// Mensaje de respuesta a CT6 (No hay que hacer nada)
 			else if (raiz == "TC7"){
 				parser_TC7(response_xml);
 			}
-			else if (raiz == "CC2"){
-				parser_CC2(response_xml);
-			}
+			// Si es cualquier otra cosa, no sabemos procesarlo y lo indicamos, devolvemos un codigo de error -1
 			else {
 				consola("ERROR: mensaje desconocido" + "Raiz Obtenida: " + raiz, "red");
-				
 				consola("Mensaje: " + response);
 				consola("Mensaje parseado: " + response_xml);
+				estado = -1;
 			}
+			
+			// Si la direccion de envio es distinta a la direccion del monitor, replicamos el mensaje
 			if (direccion !== dirMonitor){
 				var estadoMonitor = -1;
 				while (estadoMonitor == -1){
@@ -202,13 +268,14 @@ function sender(direccion, mensaje, dirMonitor) {
 				}
 
 			}
-			//El estado sigue a cero, por lo tanto todo va bien
 			
 		},
 
+		// Funcion que se ejecuta, si no se ha recibido un mensaje de error del sistema de envio
 		error: function(response) {
 			consola("Error " + response.status + ": " + response.statusText, "red");
 			consola(response.responseText);
+			// Nos guardamos el codigo de error
 			estado=response.status;
 		}
 	});
@@ -239,8 +306,7 @@ function replicador(urlMonitor, mensaje){
 			consola("Fallo envio monitor!", "red");
 			consola("Error " + response.status + ": " + response.statusText, "red");
 			consola(response.responseText);
-			estado=2; //Código de error 2: el mensaje no ha llegado al monitor
-			//return estado;
+			estado=-1; 
 		}
 	});
 	return estado;
